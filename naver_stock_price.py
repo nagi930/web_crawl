75d5f29084c9884f1e4de62d1fb5ea9208d0d4a6
nagi930 <gnkim827@gmail.com>
2021-03-18 23:14:38 +0900

    add len, getitem, getattr built-in functions

--- naver_stock_price.py
+++ naver_stock_price.py
@@ -17,16 +17,37 @@ def make_dir(dir):
         return
 
 class StockPrice:
+    stock_items = {
+        '005930': '삼성전자', '051910': 'LG화학', '063160': '종근당바이오', '006400': '삼성SDI', '185750': '종근당',
+        '006980': '우성사료', '096770': 'SK이노베이션', '035720': '카카오', '214390': '경보제약', '011200': 'HMM',
+    }
+    stock_names = [name for name in stock_items.values()]
+
     def __init__(self, code_list, start=1, end=5):
         self.code_list = code_list
         self.start = start
         self.end = end
         self.sep = end - start + 1
         self.item = None
+        self.item_list = []
         self.dfs = []
 
+    def __len__(self):
+        return len(self.dfs)
+
+    def __getitem__(self, index):
+        return self.dfs[index]
+
+    def __getattr__(self, name):
+        cls = type(self)
+        if name in cls.stock_names and name in self.item_list:
+            index = self.item_list.index(name)
+            return self.dfs[index]
+        raise AttributeError(f'{cls.__name__} object has no attribute {name}')
+
     def make_dataframe(self):
-        url_list = [(code, f'https://finance.naver.com/item/sise_day.nhn?code={code}&page={page}')
+        url_list = [(StockPrice.stock_items[code],
+                    f'https://finance.naver.com/item/sise_day.nhn?code={code}&page={page}')
                     for code in self.code_list
                     for page in range(self.start, self.end+1)]
         url_list = deque(url_list)
@@ -44,6 +65,7 @@ class StockPrice:
                     return
                 df_list.append(pd.read_html(html)[0].dropna())
 
+            self.item_list.append(self.item)
             df = pd.concat(df_list)
             df.insert(loc=0, column='종목코드', value=self.item)
             self.dfs.append(df.sort_values(by='날짜', ascending=True).set_index('날짜'))
@@ -68,3 +90,8 @@ if __name__ == '__main__':
         print('-' * 100)
 
     stocks.save_dataframe('./source/dataframe')
+
+    print(stocks[4])
+    print(stocks.삼성전자)
+    # print(stocks.서울식품)
+
